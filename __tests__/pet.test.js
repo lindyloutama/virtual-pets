586d9de3cd58ddfd1ca95dcff0e0b140c49684ad
Linda <linda.tamakaha@gmail.com>
2018-11-25 03:53:00 +0000

    adding hunger, fitness

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -22,4 +22,34 @@ describe('constructor', () => {
       expect(pet.age).toEqual(1);
     });
   });
-  
+
+  describe('constructor', () => {
+    it('has initial hunger of 0', () => {
+      const pet = new Pet('Fido');
+      expect(pet.hunger).toEqual(0);
+    });
+  });
+
+  describe('hunger', () => {
+    it('grow up should increase the hunger ', () => {
+      const pet = new Pet('Fido');
+      pet.growUp(1);
+      expect(pet.hunger).toEqual(5);
+    });
+  });
+
+  describe('constructor', () => {
+    it('has initial fitness of 10', () => {
+      const pet = new Pet('Fido');
+      expect(pet.fitness).toEqual(10);
+    });
+  });
+
+
+  describe('fitness', () => {
+    it('growUp should decrease fitness by 3 ', () => {
+      const pet = new Pet('Fido');
+      pet.growUp(5);
+      expect(pet.fitness).toEqual(7);
+    });
+  });
