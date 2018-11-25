c8096c86dd2d7f16c9a8ea036a993ed9f87a53af
Linda <linda.tamakaha@gmail.com>
2018-11-25 02:37:29 +0000

    adding gitingnore

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -1,8 +1,8 @@
 const Pet = require('../src/pet');
 
 describe('constructor', () => {
-    it('returns an object', () => {
-      expect(new Pet('Fido')).toBeInstanceOf(Object);
+    it('sets the name property', () => {
+      const pet = new Pet('Fido');
+      expect(pet.name).toEqual('Fido');
     });
   });
-  
